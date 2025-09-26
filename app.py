4b7fa1cd3aaee0fb2ce86de10ec1b65d6a8da57d
uppara <upparavinay2003n@gmail.com>
2025-09-26 16:44:55 +0530

    addapp.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# Simple Python program
+
+# Print a message
+print("Hello! Welcome to Python.")
+
+# Take user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Do a small calculation
+next_year_age = age + 1
+
+# Show output
+print(f"Hi {name}, you are {age} years old.")
+print(f"Next year, you will be {next_year_age} years old!")
+
